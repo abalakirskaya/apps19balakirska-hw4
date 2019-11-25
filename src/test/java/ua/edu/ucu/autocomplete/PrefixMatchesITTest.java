a432487311f84f347fff312e3ab54d37fb6dc40a
alisa <balakirska@ucu.edu.ua>
2019-11-25 02:10:57 +0200

    Last edition

--- src/test/java/ua/edu/ucu/autocomplete/PrefixMatchesITTest.java
+++ src/test/java/ua/edu/ucu/autocomplete/PrefixMatchesITTest.java
@@ -14,6 +14,7 @@ import ua.edu.ucu.tries.RWayTrie;
 public class PrefixMatchesITTest {
 
     private PrefixMatches pm;
+    private PrefixMatches list;
 
     @Before
     public void init() {
@@ -44,5 +45,45 @@ public class PrefixMatchesITTest {
 
         assertThat(result, containsInAnyOrder(expResult));
     }
+
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void test_wordsWithPrefix2() {
+        String pref = "abce";
+        int k = 1;
+
+        Iterable<String> result = pm.wordsWithPrefix(pref, k);
+
+
+    }
+    @Test
+    public void testWordsWithPrefix_2() {
+        String pref = "abc";
+        int k = 2;
+
+        Iterable<String> result = pm.wordsWithPrefix(pref, k);
+
+        String[] expResult = {"abc", "abce", "abcd"};
+
+        assertThat(result, containsInAnyOrder(expResult));
+    }
+
+    @Test
+    public void testWordsWithPrefix_less_than_2() {
+        String pref = "a";
+        int k = 2;
+        list = new PrefixMatches(new RWayTrie());
+        list.load("a", "b", "c", "d");
+
+        Iterable<String> result = list.wordsWithPrefix(pref, k);
+
+        String[] expResult = {};
+
+        assertThat(result, containsInAnyOrder(expResult));
+    }
+
+
+
+
 
 }
